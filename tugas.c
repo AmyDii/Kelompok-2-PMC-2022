b9637f22e91cb1d4c3a9745aee5aa2244003f9ee
Eraraya Morenzo Muten <morenzomuten@gmail.com>
2022-03-22 16:08:01 +0700

    code template

--- tugas.c
+++ tugas.c
@@ -1,3 +1,26 @@
+/*EL2008 Pemecahan Masalah dengan C 2021/2022
+*Kelas            : 1
+*Tugas            : 5
+*Kelompok         : 2
+*Hari dan Tanggal : Kamis, 17 Maret 2022
+*Nama (NIM)       : Eraraya Morenzo Muten (18320003)
+                    Michelle Angelina (18320007)
+                    Shadrina Syahla Vidyana (18320031)
+*Nama File        : tugas.c
+*Deskripsi        : Program perkalian matriks
+*/
+
+
 //tada
 //Buatlah sebuah kelompok yg beranggotakan dari 3 orang, lalu buat program perkalian matrik untuk ukuran 10x10, 100x100, 1000x1000, 
 //10000x10000, lakukan perkalian dgn beberapa algoritma
+
+// Kamus
+#include <stdio.h>
+#include <stdlib.h>
+
+// Program
+int main{
+    
+    return 0;
+}
