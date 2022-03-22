ff96b69b33e17543e6e3e19bbae308d310904004
AmyDii <82463378+AmyDii@users.noreply.github.com>
2022-03-22 14:02:32 +0700

    Update tugas.c

--- tugas.c
+++ tugas.c
@@ -1,2 +1,3 @@
 //tada
-
+//Buatlah sebuah kelompok yg beranggotakan dari 3 orang, lalu buat program perkalian matrik untuk ukuran 10x10, 100x100, 1000x1000, 
+//10000x10000, lakukan perkalian dgn beberapa algoritma
